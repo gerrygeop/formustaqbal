43998fb6a72f0506272fc560340758f66e8929c7
gerrygeop <gerryja28@gmail.com>
2023-08-11 11:43:51 +0800

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,9 +17,17 @@ export default {
             colors: {
                 accent: "#D4B157",
                 oren: "#FDB82D",
+
+                richblack: "#141A22ff",
+                raisinblack: "#191F28ff",
+                lion: "#C69F68ff",
+                ashgray: "#B6B7A4ff",
+                yimblue: "#4B5975ff",
             },
         },
     },
+
+    darkMode: "class",
 
     plugins: [forms],
 };
