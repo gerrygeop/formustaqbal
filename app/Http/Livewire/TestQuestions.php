e56dc49d2431fc3fd4888c7d8f713d56a79de27e
gerrygeop <gerryja28@gmail.com>
2023-08-16 01:15:25 +0800

    livewire component test questions

---
+++ app/Http/Livewire/TestQuestions.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Answer;
+use App\Models\Test;
+use Illuminate\Support\Facades\DB;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+
+class TestQuestions extends Component
+{
+    use WithFileUploads;
+
+    public $test;
+    public $questions;
+    public $questionNull = false;
+    public $currentQuestion;
+    public $questionIndex = 0;
+    public $answers = [];
+
+    protected $messages = [
+        'answers.*.speaking' => 'The Audio file must be a file of type: audio/mpeg, audio/ogg.',
+    ];
+
+    public function mount(Test $test)
+    {
+        $this->test = $test;
+        $this->loadQuestions();
+    }
+
+    public function loadQuestions()
+    {
+        $this->questions = $this->test->questions()
+            ->with('choices')
+            ->get();
+
+        if ($this->questions->isNotEmpty()) {
+            $this->currentQuestion = $this->questions[0];
+        }
+    }
+
+    public function render()
+    {
+        return view('livewire.test-questions', [
+            'question' => $this->currentQuestion
+        ]);
+    }
+
+    public function nextQuestion()
+    {
+        if ($this->saveAnswer($this->currentQuestion->id)) {
+            $currentIndex = $this->questions->search(function ($question) {
+                return $question->id === $this->currentQuestion->id;
+            });
+
+            if ($currentIndex !== false && $currentIndex < $this->questions->count() - 1) {
+                $this->questionIndex = $currentIndex + 1;
+                $this->currentQuestion = $this->questions[$currentIndex + 1];
+            }
+        }
+    }
+
+    public function saveAnswer($questionId)
+    {
+        if (!isset($this->answers[$questionId])) {
+            $this->questionNull = true;
+            return false;
+        } else {
+            $this->questionNull = false;
+            $response = [
+                'question_id' => $questionId,
+                'question_type' => $this->currentQuestion->type,
+            ];
+
+            if ($this->currentQuestion->type == 1) {
+                $response['response'] = $this->answers[$questionId];
+            } else {
+                if ($this->currentQuestion->type == 4 && isset($this->answers[$questionId]['speaking'])) {
+
+                    $this->validate([
+                        'answers.*.speaking' => 'file|mimetypes:audio/mpeg,audio/ogg|max:5024', // 5MB Max
+                    ]);
+
+                    $audioFile = $this->answers[$questionId]['speaking'];
+                    $audioFileName = $audioFile->store('speaking-test', 'public');
+                    $response['response'] = $audioFileName;
+                } else {
+                    $response['response'] = $this->answers[$questionId];
+                }
+            }
+
+            $this->answers[$questionId] = $response;
+
+            return true;
+        }
+    }
+
+    public function submitAnswers()
+    {
+        if ($this->saveAnswer($this->currentQuestion->id)) {
+            DB::transaction(function () {
+                $arr = [];
+                foreach ($this->answers as $value) {
+                    $arr[] = $value;
+                }
+
+                $answer = new Answer();
+                $answer->user_id = auth()->user()->id;
+                $answer->answer = json_encode($arr);
+
+                $this->test->answers()->save($answer);
+            });
+
+            return redirect()->to('/dashboard');
+        }
+    }
+}
---
+++ resources/views/livewire/test-questions.blade.php
@@ -0,0 +1,76 @@
+<div class="max-w-5xl mx-auto w-full">
+	<div class="bg-white/80 backdrop-blur border rounded-xl shadow py-8 px-10">
+
+		@if ($questions->isNotEmpty())
+			<div class="flex flex-col">
+				<div class="flex items-center justify-between mb-2">
+					<p class="text-gray-600 font-semibold">
+						Question: {{ $questionIndex + 1 }} of {{ $questions->count() }}
+					</p>
+					<p class="text-gray-600 font-semibold mb-4">
+						<x-question-type :type="$question->type" />
+					</p>
+				</div>
+
+				<div class="my-4">
+					@if ($question->file_path)
+						<div class="mb-4">
+							@if ($question->type == 3)
+								<audio controls class="bg-yellow-400" id="player">
+									<source src="{{ asset('storage/' . $question->file_path) }}" type="audio/mpeg">
+								</audio>
+							@else
+								<img src="{{ asset('storage/' . $question->file_path) }}" alt="Image">
+							@endif
+						</div>
+					@endif
+
+					<p class="text-xl lg:text-2xl text-gray-700 font-medium">
+						{{ $question->question }}
+					</p>
+				</div>
+
+				<div class="flex flex-col gap-4">
+					@if ($question->type == 1)
+						@foreach ($question->choices->options as $choice)
+							<label class="text-lg font-medium">
+								<input type="radio" name="choice_{{ $question->id }}" wire:model="answers.{{ $question->id }}"
+									value="{{ $choice['id'] }}"> {{ $choice['choice'] }}
+							</label>
+						@endforeach
+					@elseif ($question->type == 2 || $question->type == 3)
+						<x-textarea name="response_{{ $question->id }}" cols="30" rows="10"
+							placeholder="Tuliskan jawaban anda..." wire:model="answers.{{ $question->id }}" />
+					@elseif ($question->type == 4)
+						<div class="p-4 rounded-lg bg-white shadow">
+							<span class="mb-4">Upload Audio</span>
+							<input type="file" name="response_{{ $question->id }}" wire:model="answers.{{ $question->id }}.speaking"
+								class="block w-full text-sm text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100" />
+						</div>
+					@endif
+
+					@if ($questionNull)
+						<p class="text-red-500">Anda belum menjawab</p>
+					@endif
+					@error('answers.*.speaking')
+						<span class="text-red-500">{{ $message }}</span>
+					@enderror
+				</div>
+			</div>
+		@endif
+	</div>
+
+	<div class="flex justify-center mt-8">
+		@if ($questionIndex == $questions->count() - 1)
+			<button wire:click.prevent="submitAnswers"
+				class="max-w-3xl w-full bg-gray-200 py-3 px-4 text-yellow-500 font-bold uppercase border focus:border-yellow-400 rounded-lg shadow">
+				Submit
+			</button>
+		@else
+			<button wire:click.prevent="nextQuestion" type="button"
+				class="max-w-3xl w-full bg-gray-200 py-3 px-4 text-yellow-500 font-bold uppercase border focus:border-yellow-400 rounded-lg shadow">
+				Lanjut
+			</button>
+		@endif
+	</div>
+</div>
