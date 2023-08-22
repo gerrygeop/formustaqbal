5c60f4a6b374044a6a5006c4a80e89c523db676d
gerrygeop <gerryja28@gmail.com>
2023-08-22 23:38:56 +0800

    leaderboard users

---
+++ resources/views/leaderboards/leaderboard.blade.php
@@ -0,0 +1,22 @@
+<x-app-layout>
+	<div class="max-w-7xl mx-auto">
+		<div class="py-8">
+			<div class="flex mb-3">
+				<h3 class="text-lg text-slate-500 leading-0 dark:text-slate-300">Leaderboard</h3>
+			</div>
+
+			{{-- Leaderboard --}}
+			<div class="grid grid-cols-2 gap-4">
+				<div class="col-span-full bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+					<div class="p-6 text-gray-900 dark:text-gray-100">
+						<div class="flex items-center justify-center space-x-2">
+							<h2 class="text-lg text-slate-600 dark:text-gray-50">
+								Belum terdapat leaderboard
+							</h2>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</x-app-layout>
