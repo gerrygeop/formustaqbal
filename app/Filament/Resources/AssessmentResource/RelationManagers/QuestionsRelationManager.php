89bf6bfc5cf70114fe610e061cdb422ef555c5a9
gerrygeop <gerryja28@gmail.com>
2023-08-25 01:35:07 +0800

    update question resources

--- app/Filament/Resources/AssessmentResource/RelationManagers/QuestionsRelationManager.php
+++ app/Filament/Resources/AssessmentResource/RelationManagers/QuestionsRelationManager.php
@@ -19,8 +19,9 @@ class QuestionsRelationManager extends RelationManager
     {
         return $form
             ->schema([
-                Forms\Components\Card::make()->schema([
+                Forms\Components\Grid::make(1)->schema([
                     Forms\Components\Select::make('type')
+                        ->label('Jenis Soal')
                         ->required()
                         ->options([
                             '1' => 'Multiple Choice',
@@ -31,50 +32,74 @@ class QuestionsRelationManager extends RelationManager
                         ->reactive(),
 
                     Forms\Components\FileUpload::make('file_path')
-                        ->label('Audio/Image')
+                        ->label('Audio/Gambar')
                         ->directory(fn (callable $get): string => $get('type') == 3 ? 'question-audio' : 'question-images')
-                        ->acceptedFileTypes(['audio/mpeg', 'audio/ogg', 'audio/wav', 'image/jpeg', 'image/png', 'image/webp'])
+                        ->acceptedFileTypes(['audio/mpeg', 'audio/ogg', 'image/jpeg', 'image/png', 'image/webp'])
                         ->imageResizeMode('cover')
                         ->imagePreviewHeight('200')
                         ->enableOpen()
+                        ->enableDownload()
                         ->maxSize(3024),
 
-                    Forms\Components\Textarea::make('question'),
-                    Forms\Components\TextInput::make('point')->numeric()->default(0)->minValue(0),
-                ]),
+                    Forms\Components\RichEditor::make('question')
+                        ->label('Pertanyaan')
+                        ->toolbarButtons([
+                            'bold',
+                            'italic',
+                            'underline',
+                        ]),
 
-                Forms\Components\Section::make('Options')
-                    ->schema([
-                        Forms\Components\Repeater::make('choices')
-                            ->relationship('choices')
-                            ->schema([
-                                Forms\Components\FileUpload::make('image_path')
-                                    ->label('Image')
-                                    ->directory('choices-images')
-                                    ->image()
-                                    ->imageResizeMode('cover')
-                                    ->imagePreviewHeight('200')
-                                    ->enableOpen()
-                                    ->maxSize(3024)
-                                    ->columnSpan([
-                                        'md' => 3,
-                                    ]),
-                                Forms\Components\TextInput::make('choice')->label('Choice Text')->columnSpan([
-                                    'md' => 5,
+                    Forms\Components\Grid::make(3)->schema([
+                        Forms\Components\TextInput::make('point')
+                            ->numeric()
+                            ->default(0)
+                            ->minValue(0),
+                        Forms\Components\Toggle::make('is_choice_rtl')
+                            ->label('Teks Pilihan Dari Kanan')
+                            ->inline(false)
+                            ->default(false)
+                            ->visible(fn (callable $get): bool => $get('type') == 1 ? true : false),
+                        Forms\Components\Toggle::make('is_random_choices')
+                            ->label('Acak Pilihan')
+                            ->inline(false)
+                            ->default(false)
+                            ->visible(fn (callable $get): bool => $get('type') == 1 ? true : false),
+                    ]),
+
+                    Forms\Components\Repeater::make('choices')
+                        ->relationship('choices')
+                        ->schema([
+                            Forms\Components\FileUpload::make('image_path')
+                                ->label('Gambar (max: 3mb)')
+                                ->directory('choices-images')
+                                ->image()
+                                ->imageResizeMode('cover')
+                                ->imagePreviewHeight('200')
+                                ->enableOpen()
+                                ->enableDownload()
+                                ->maxSize(3024)
+                                ->columnSpan([
+                                    'md' => 3,
                                 ]),
-                                Forms\Components\Toggle::make('is_correct')->inline(false)->columnSpan([
-                                    'md' => 2
+                            Forms\Components\TextInput::make('choice')->label('Teks')->columnSpan([
+                                'md' => 8,
+                            ]),
+                            Forms\Components\Toggle::make('is_correct')
+                                ->label('Benar')
+                                ->inline(false)
+                                ->columnSpan([
+                                    'md' => 1
                                 ]),
-                            ])
-                            ->disableLabel()
-                            ->columns([
-                                'md' => 10,
-                            ])
-                            ->createItemButtonLabel('Add Option'),
-                    ])
-                    ->visible(function (callable $get) {
-                        return ($get('type') == '1') ? true : false;
-                    }),
+                        ])
+                        ->label('Pilihan')
+                        ->columns([
+                            'md' => 12,
+                        ])
+                        ->createItemButtonLabel('Tambah Pilihan')
+                        ->visible(function (callable $get) {
+                            return ($get('type') == '1') ? true : false;
+                        }),
+                ]),
             ]);
     }
 
@@ -82,21 +107,31 @@ class QuestionsRelationManager extends RelationManager
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('type')->enum([
-                    '1' => 'Multiple Choice',
-                    '2' => 'Essay',
-                    '3' => 'Listenig',
-                    '4' => 'Speaking',
-                ]),
-                Tables\Columns\TextColumn::make('question')->words(10)->searchable(),
+                Tables\Columns\TextColumn::make('type')
+                    ->label('Jenis Soal')
+                    ->enum([
+                        '1' => 'Multiple Choice',
+                        '2' => 'Essay',
+                        '3' => 'Listenig',
+                        '4' => 'Speaking',
+                    ]),
+                Tables\Columns\TextColumn::make('question')
+                    ->label('Pertanyaan')
+                    ->words(10)
+                    ->searchable()
+                    ->placeholder('-')
+                    ->html(),
+                Tables\Columns\TextColumn::make('point')->sortable(),
             ])
             ->filters([
-                Tables\Filters\SelectFilter::make('type')->options([
-                    '1' => 'Multiple Choice',
-                    '2' => 'Essay',
-                    '3' => 'Listening',
-                    '4' => 'Speaking',
-                ]),
+                Tables\Filters\SelectFilter::make('type')
+                    ->label('Jenis Soal')
+                    ->options([
+                        '1' => 'Multiple Choice',
+                        '2' => 'Essay',
+                        '3' => 'Listening',
+                        '4' => 'Speaking',
+                    ]),
             ])
             ->headerActions([
                 Tables\Actions\CreateAction::make(),
