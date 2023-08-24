29da36a9b8c7f3aa94460d280b1bcede626d6ccf
gerrygeop <gerryja28@gmail.com>
2023-08-25 01:33:43 +0800

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,
             FacultySeeder::class,
-            // SiaSeeder::class,
+            SiaSeeder::class,
         ]);
 
         DB::table('role_user')->insert([
