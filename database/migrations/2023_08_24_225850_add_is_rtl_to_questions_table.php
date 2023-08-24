e9f20edf8b9ca8e533efee2e240262aebd0d3b88
gerrygeop <gerryja28@gmail.com>
2023-08-25 01:33:32 +0800

    add column to questions

---
+++ database/migrations/2023_08_24_225850_add_is_rtl_to_questions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->boolean('is_choice_rtl')->default(false);
+            $table->boolean('is_random_choices')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('questions', function (Blueprint $table) {
+            $table->dropColumn('is_choice_rtl');
+            $table->dropColumn('is_random_choices');
+        });
+    }
+};
