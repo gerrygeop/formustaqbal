ce883ae0df69e0309b5fb83fa71cb6f5f22d144d
gerrygeop <gerryja28@gmail.com>
2023-08-30 05:27:28 +0800

    room view

---
+++ resources/views/teachers/room.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+	<div class="max-w-7xl mx-auto">
+		<div class="py-8">
+			<div class="flex mb-3">
+				<h3 class="text-lg text-slate-800 leading-0 dark:text-slate-100">Siswa</h3>
+			</div>
+
+			<div class="grid grid-cols-2 gap-4">
+				<div class="col-span-full bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+					<div class="p-6 text-gray-900 dark:text-gray-100">
+						<div class="flex items-center justify-center space-x-2">
+							<p>Belum terdapat siswa</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</x-app-layout>
