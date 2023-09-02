fe0c398bd3f91289c67d9d359f765e5785ebaf4a
gerrygeop <gerryja28@gmail.com>
2023-09-03 05:19:57 +0800

    module controller

---
+++ app/Http/Controllers/ModuleController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Module;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ModuleController extends Controller
+{
+    public function show(Module $module)
+    {
+        $module->load('course', 'submodules');
+
+        $user = auth()->user();
+        $courseUser = DB::table('course_user')
+            ->where('module_id', $module->id)
+            ->where('course_id', $module->course->id)
+            ->where('user_id', $user->id)
+            ->first();
+
+        // $lastVisitSubmodule = $courseUser->last_visit;
+        $completedSubmodules = json_decode($courseUser->completed_submodules);
+
+        return view('modules.show', [
+            'module' => $module,
+            'course' => $module->course,
+            'completedSubmodules' => $completedSubmodules,
+        ]);
+    }
+}
