e9317f07b5bf0976fae7d6d46c47329671cbce7d
gerrygeop <gerryja28@gmail.com>
2023-09-10 02:12:56 +0800

    quiz controller

---
+++ app/Http/Controllers/QuizController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AssessmentUser;
+use App\Models\Chapter;
+use App\Models\Module;
+use Carbon\Carbon;
+
+class QuizController extends Controller
+{
+    public function quiz(Module $module, Chapter $chapter)
+    {
+        $module->load([
+            'users' => function ($query) {
+                $query->where('user_id', auth()->id());
+            },
+        ]);
+
+        if (!in_array($chapter->id, json_decode($module->users->first()->pivot->completed_submodules))) {
+            abort(403);
+        }
+
+        $chapter->load(['assessment' => function ($query) {
+            $query->where('is_active', true)
+                ->where([
+                    ['published_at', '<=', Carbon::now()],
+                ])
+                ->with(['users' => function ($query) {
+                    $query->where('user_id', auth()->id());
+                }]);
+        }]);
+        $assessment = $chapter->assessment;
+
+        if ($assessment->users->count() == 0) {
+            $au = AssessmentUser::create([
+                'user_id' => auth()->id(),
+                'assessment_id' => $assessment->id,
+            ]);
+        } else {
+            $au = $assessment->users->first()->pivot;
+        }
+
+        if ($au->is_completed == 1 || $au->created_at->addMinutes($assessment->duration_minutes) < now()) {
+            return to_route('courses.learn', [$chapter->submodule->module->id, $chapter]);
+        } else {
+            return view('quizzes.assessment-layout', [
+                'chapter' => $chapter,
+                'assessment' => $assessment,
+            ]);
+        }
+    }
+}
