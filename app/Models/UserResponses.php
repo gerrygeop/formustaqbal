3b257efd43507c93cc07de14804139754d2c6e53
gerrygeop <gerryja28@gmail.com>
2023-09-10 02:11:46 +0800

    added model & migration: user responses

---
+++ app/Models/UserResponses.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserResponses extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+    protected $table = 'user_responses';
+
+    protected $casts  = [
+        'responses' => 'json',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function assessment(): BelongsTo
+    {
+        return $this->belongsTo(Assessment::class);
+    }
+}
