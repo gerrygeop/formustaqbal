b2ed1a374e650d840765574bac0f3f669bf4f5e5
gerrygeop <gerryja28@gmail.com>
2023-09-14 05:09:44 +0800

    added module_id column to room table

---
+++ database/migrations/2023_09_14_013325_add_module_id_column_to_rooms_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('rooms', function (Blueprint $table) {
+            $table->foreignId('module_id')->after('id')->nullable()->constrained('modules')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('rooms', function (Blueprint $table) {
+            $table->dropColumn('module_id');
+        });
+    }
+};
