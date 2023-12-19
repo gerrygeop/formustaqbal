d947fe4b352dc407bcdc376784f36ea87081e079
gerrygeop <gerryja28@gmail.com>
2023-12-20 00:17:00 +0800

    update component th

--- resources/views/components/th.blade.php
+++ resources/views/components/th.blade.php
@@ -1,4 +1,5 @@
 <th scope="col"
-	class="px-4 py-3.5 font-medium text-xs text-left text-gray-500 dark:text-gray-400 rtl:text-right uppercase tracking-wide">
+	class="px-4 py-3.5 font-medium text-xs text-left text-gray-500 dark:text-gray-400 rtl:text-right uppercase tracking-wide"
+	{{ $attributes }}>
 	{{ $slot }}
 </th>
