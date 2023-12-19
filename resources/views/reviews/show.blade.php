f1657b613bea85dca79464b01590abdca1556172
gerrygeop <gerryja28@gmail.com>
2023-12-20 00:21:59 +0800

    update show review

--- resources/views/reviews/show.blade.php
+++ resources/views/reviews/show.blade.php
@@ -32,6 +32,21 @@
 					<div class="max-w-5xl mx-auto">
 						<div class="py-10 px-4 lg:px-0 mb-20">
 
+							@if (session('success-delete-response'))
+								<div role="alert" x-show="alert"
+									class="mb-6 w-full flex items-center px-4 py-4 text-base text-green-800 bg-green-100 rounded-md ring-1 ring-inset ring-green-600/20">
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+										stroke="currentColor" class="w-6 h-6 mr-2">
+										<path stroke-linecap="round" stroke-linejoin="round"
+											d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+									</svg>
+
+									<div>
+										{{ session('success-delete-response') }}
+									</div>
+								</div>
+							@endif
+
 							<h2 class="font-semibold text-2xl text-gray-700">
 								{{ $assessment->title }}
 							</h2>
@@ -40,7 +55,7 @@
 								@slot('thead')
 									<x-th>No</x-th>
 									<x-th>Waktu</x-th>
-									<x-th>Score</x-th>
+									<x-th>Skor</x-th>
 									<x-th>Status</x-th>
 									<th scope="col" class="relative py-3.5 px-4">
 										<span class="sr-only">Edit</span>
@@ -67,10 +82,21 @@
 												@endif
 											</x-td>
 											<x-td>
-												<a href="{{ route('courses.review.edit', [$module, $assessment, $response]) }}"
-													class="px-3 py-1 text-amber-600 dark:text-gray-300 hover:underline transition-all duration-200">
-													Review
-												</a>
+												<div class="flex items-center gap-x-1">
+													<a href="{{ route('courses.review.edit', [$module, $assessment, $response]) }}"
+														class="px-3 py-1 text-amber-600 dark:text-gray-300 hover:underline transition-all duration-200">
+														Review
+													</a>
+
+													<form action="{{ route('courses.review.destroy', $response) }}" method="POST">
+														@csrf
+														@method('DELETE')
+
+														<button class="px-3 py-1 text-red-600 dark:text-gray-300 hover:underline transition-all duration-200">
+															Hapus
+														</button>
+													</form>
+												</div>
 											</x-td>
 										</tr>
 									@endforeach
